e93b82d01968fffe7e10e1bfd96d159f0b6101c1
bandstrar <rob.bandstra@gmail.com>
2020-08-27 20:51:55 -0500

    created modal that is printing to the dom for each card

--- javascripts/components/dinoButtons.js
+++ javascripts/components/dinoButtons.js
@@ -1,20 +1,29 @@
 import { printDinoCards } from './dinoCards.js'
 import { adventures, dinos } from '../data/data.js'
+import { buildModal } from './dinoModal.js'
 
 
 const dinoButtons = (dino) => {
+    $(`#image${dino.id}`).on('click', () => {
+        $('#dinoModal').append(
+        buildModal(dino)
+        )
+    })
+
     $(`#food${dino.id}`).on('click', () => {
         if (dino.health < 100) {
         dino.health += 10;
         printDinoCards();
         }
     })
+
     $(`#pet${dino.id}`).on('click', () => {
         if (dino.health < 100) {
         dino.health += 5;
         printDinoCards();
         }
     })
+
     $(`#adventure${dino.id}`).on('click', () => {
         const randomAdventure = Math.floor(Math.random() * Math.floor(adventures.length))
         const failOrSuccess = Math.floor(Math.random() * Math.floor(100))
@@ -28,6 +37,7 @@ const dinoButtons = (dino) => {
     }
         printDinoCards();
     })
+
     $(`#remove${dino.id}`).on('click', () => {
         const findId = dinos.indexOf(dino)
         dinos.splice(findId, 1);
--- javascripts/components/dinoCards.js
+++ javascripts/components/dinoCards.js
@@ -1,5 +1,6 @@
 import { dinos } from '../data/data.js'
 import { dinoButtons } from './dinoButtons.js'
+import { dinoHealth } from './healthBar.js'
 
 const printDinoCards = () => {
     dinos.forEach((dino) => {
@@ -8,37 +9,33 @@ const printDinoCards = () => {
         $('#dinoKennel').append(
             dinoCards(dino)
         )
-        $(`#progress${dino.id}`).addClass('bg-success')
         } else if (dino.health > 0 && dino.health <= 30) {
             $(`#dino${dino.id}`).remove();
            $('#dinoHospital').append(
             dinoCards(dino)
            ) 
-           $(`#progress${dino.id}`).addClass('bg-danger')
         } else {
             $(`#dino${dino.id}`).remove();
             $('#dinoGraveyard').append(
                 dinoCards(dino)
             )
-            $(`#progressContainer${dino.id}`).replaceWith('<i class="fas fa-skull-crossbones"></i>')
             }
             dinoButtons(dino);
     })
 }
 
 const dinoCards = (dino) => {
-    return `<div class="card" style="width: 18rem;" id="dino${dino.id}">
-    <img src="${dino.imageUrl}" class="card-img-top" alt="${dino.name}">
+    return `<div class="card m-3" style="width: 18rem;" id="dino${dino.id}">
+    <button type="button" data-toggle="modal" data-target="#dinoModal${dino.id}" class="image-button" id="image${dino.id}">
+    <img src="${dino.imageUrl}" class="card-img-top" alt="${dino.name}"></button>
     <div class="card-body">
-      <h5 class="card-title">${dino.name}</h5>
-      <div id="progressContainer${dino.id}" class="progress">
-        <div id="progress${dino.id}" class="progress-bar progress-bar-striped" role="progressbar" style="width: ${dino.health}%" aria-valuenow="${dino.health}" aria-valuemin="0" aria-valuemax="100">${dino.health}%</div>
-      </div>
-      <div id="buttonContainer${dino.id}">
-        <button type="submit" id="food${dino.id}"><i class="fas fa-utensils"></i></button>
-        <button type="submit" id="pet${dino.id}"><i class="fas fa-hand-sparkles"></i></button>
-        <button type="submit" id="adventure${dino.id}"><i class="fas fa-hiking"></i></button>
-        <button type="submit" id="remove${dino.id}"><i class="fas fa-minus-circle"></i></button>
+    <h5 class="card-title">${dino.name}</h5>
+      ${dinoHealth(dino)}
+      <div id="buttonContainer${dino.id}" class="d-flex flex-wrap">
+        <button type="submit" id="food${dino.id}" class="w-25 mt-1 mr-4 btn-success"><i class="fas fa-utensils"></i></button>
+        <button type="submit" id="pet${dino.id}" class="w-25 mt-1 ml-5 btn-warning"><i class="fas fa-hand-sparkles"></i></button>
+        <button type="submit" id="adventure${dino.id}" class="w-25 mt-1 mr-4 btn-secondary"><i class="fas fa-hiking"></i></button>
+        <button type="submit" id="remove${dino.id}" class="w-25 mt-1 ml-5 btn-danger"><i class="fas fa-minus-circle"></i></button>
         </div>
     </div>
   </div>`
