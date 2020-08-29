b7055ecfdaa362ddaf33a7e9d5231a4fcfb00f7e
bandstrar <rob.bandstra@gmail.com>
2020-08-29 10:17:39 -0500

    added function that decrements the dinos health over time

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import { addADino } from './javascripts/components/addDino.js'
 import { printDinoCards } from './javascripts/components/dinoCards.js';
+import { dyingDinos } from './javascripts/components/decrement.js'
 
 const init = () => {
 addADino();
 printDinoCards();
+dyingDinos();
 }
 
 init();
